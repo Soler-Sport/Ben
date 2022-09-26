b18cd26a4c75924474547a742d85c2e3979838a1
Soler-Sport <102955989+Soler-Sport@users.noreply.github.com>
2022-09-26 17:19:19 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+from types import MemberDescriptorType
+import discord
+import random
+
+no = "https://c.tenor.com/hvtkROeQzQgAAAAC/ben-no.gif"
+yes = "https://c.tenor.com/evyoapv3BnsAAAAC/ben-yes.gif"
+ho_ho = "https://c.tenor.com/SLBbnychkCkAAAAC/ben-ho-ho-ho.gif"
+ugh = "https://c.tenor.com/d2itSSm1PRsAAAAC/ben-ugh.gif"
+reactoin = [no, yes, ho_ho, ugh]
+
+intents = discord.Intents.all()
+intents.typing = True
+intents.presences = True
+intents.members = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print("Bot started")
+
+@client.event
+async def on_message(ctx):
+    if ctx.content.startswith('Бен'):
+        react = random.choice(reactoin)
+        await ctx.channel.send(react)
+        print("Ben say", react)
+        
+@client.event
+async def on_message(ctx):
+    if ctx.content.startswith('gachi'):
+        await ctx.channel.send("https://media-cdn.tripadvisor.com/media/photo-p/1b/41/a3/eb/hey-bro-nice-dick.jpg")
+
+client.run("MTAxOTk2MDM3NDI3NzE4NTU4OA.GrlQ4G.0v1TIWvaX7Vpb0qlGVHTFn4qbA9X-CMMtmypNI")
